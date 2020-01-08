60788a9c7bef64e1885046f5a5df37c90ea7ecf3
Danny-Dasilva <yahchayildasilva@gmail.com>
2020-01-07 23:45:00 -0500

    added programatic visualization

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -4,33 +4,63 @@ from PIL import Image
 import cv2
 import numpy as np
 
-img = "0-1y-1p.png"
-image = cv2.imread(img)
-height, width, channels = image.shape
-img = image
+import json
+
+with open('labels.json') as data_file:
+    data = json.load(data_file)
+
+
+# img = "0-1y-1p.png"
+# image = cv2.imread(img)
+# height, width, channels = image.shape
+# img = image
+
+
+for object in data:
+    img = object['image']
+    obj = object['meshes']
+    image = cv2.imread(img)
+    height, width, channels = image.shape
+    img = image
+    for shape in obj:
+        print(obj[shape])
+        shape = obj[shape]
+        x1 = shape['x1'] * width
+        x2 = shape['x2'] * width
+
+        y1 = shape['y1'] * height 
+        y2 = shape['y2'] * height 
+        y1 = width - y1
+        y2 = width - y2
+        cv2.rectangle(img,(int(x1),int(y1)),(int(x2),int(y2)),(0,0,255),1)
+
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+
+
+
 #img = np.zeros((500, 500, 3), np.uint8)
 
-red = [0,0,255]
-x1 =  0.7887953908949934 * width
-x2 = 0.8806406091209397 * width
+# red = [0,0,255]
+# x1 =  0.7887953908949934 * width
+# x2 = 0.8806406091209397 * width
 
-y1 = 0.6334476262158504 * width 
-y2 = 0.7133784295793628 * width 
-y1 = width - y1
-y2 = width - y2
-print(x1, y1, x2, y2, width)
-
-
+# y1 = 0.6334476262158504 * width 
+# y2 = 0.7133784295793628 * width 
+# y1 = width - y1
+# y2 = width - y2
+# print(x1, y1, x2, y2, width)
 
 
-lists = [[int(x1),int(y1)], [int(x2),int(y2)]]
-for i in range(len(lists)):
-    s, i = lists[i]
-    print(s, i)
-    img[i, s] = red
-    
-cv2.imshow('img', img)
-cv2.waitKey(0)
+
+
+
+# cv2.rectangle(img,(int(x1),int(y1)),(int(x2),int(y2)),(0,0,255),1)
+
+# cv2.imshow('image',img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
 
 
 
